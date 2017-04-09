38f442644c396f64afa14db5dbbe92733cd12ad9
Jeremy Chone <jeremy.chone@gmail.com>
2017-04-08 17:53:03 -0700

    . add the optional port number arg, like 'npm start 8888'

--- mock-server/start.js
+++ mock-server/start.js
@@ -1,5 +1,14 @@
 var server = require('./server.js');
 
-server.init().then(function(){
+
+// get the eventual port from the argv
+var params = (process.argv.length >= 3)? process.argv.slice(2): [];
+var opts = {};
+if (params.length > 0){
+	opts.port = params[0];
+}
+
+
+server.init(opts).then(function(){
 	server.start();
 });
